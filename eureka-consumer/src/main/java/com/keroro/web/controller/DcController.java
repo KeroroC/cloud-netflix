ae0ea69b1229392dfbd4c11b6e549ff4bd06718c
Keroro <keroro714@qq.com>
2024-01-22 22:50:31 +0800

    feat(eurekaConsumer): 增加ribbon依赖，通过ribbon实现客户端负载均衡

--- eureka-consumer/src/main/java/com/keroro/web/controller/DcController.java
+++ eureka-consumer/src/main/java/com/keroro/web/controller/DcController.java
@@ -1,7 +1,5 @@
 package com.keroro.web.controller;
 
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -13,21 +11,14 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class DcController {
 
-    private final LoadBalancerClient loadBalancerClient;
-
     private final RestTemplate restTemplate;
 
-    public DcController(LoadBalancerClient loadBalancerClient, RestTemplate restTemplate) {
-        this.loadBalancerClient = loadBalancerClient;
+    public DcController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @GetMapping("/consumer")
     public String dc() {
-        // 通过负载均衡选出一个服务器实例
-        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
-        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/dc";
-        System.out.println(url);
-        return restTemplate.getForObject(url, String.class);
+        return restTemplate.getForObject("http://eureka-client/dc", String.class);
     }
 }
