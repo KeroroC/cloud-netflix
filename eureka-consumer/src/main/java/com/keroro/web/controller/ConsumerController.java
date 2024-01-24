278809a6b04ce635d83ea2f90917697817ccfe06
Keroro <keroro714@qq.com>
2024-01-24 19:09:11 +0800

    feat(eureka-consumer): 测试从配置中心读取加密的配置文件

--- eureka-consumer/src/main/java/com/keroro/web/controller/ConsumerController.java
+++ eureka-consumer/src/main/java/com/keroro/web/controller/ConsumerController.java
@@ -1,6 +1,7 @@
 package com.keroro.web.controller;
 
 import com.keroro.web.feign.DcClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,9 @@ public class ConsumerController {
 
     private final DcClient dcClient;
 
+    @Value("${info.databasePwd}")
+    private String value;
+
     public ConsumerController(DcClient dcClient) {
         this.dcClient = dcClient;
     }
@@ -20,5 +24,10 @@ public class ConsumerController {
     @GetMapping("/consumer")
     public String dc() {
         return dcClient.consumer();
+    }
+
+    @GetMapping("/pwd")
+    public String printConfiguration() {
+        return value;
     }
 }
