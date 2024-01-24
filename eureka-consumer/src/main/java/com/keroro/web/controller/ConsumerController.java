bb5ab517769851471c143e57b3860144f7cbcf45
Keroro <keroro714@qq.com>
2024-01-24 21:42:23 +0800

    feat(eureka-consumer): 通过actuator实现配置的动态刷新

--- eureka-consumer/src/main/java/com/keroro/web/controller/ConsumerController.java
+++ eureka-consumer/src/main/java/com/keroro/web/controller/ConsumerController.java
@@ -2,6 +2,7 @@ package com.keroro.web.controller;
 
 import com.keroro.web.feign.DcClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024年01月22日 22:09
  */
 @RestController
+@RefreshScope
 public class ConsumerController {
 
     private final DcClient dcClient;
 
     @Value("${info.databasePwd}")
     private String value;
+
+    @Value("${info.name}")
+    private String name;
 
     public ConsumerController(DcClient dcClient) {
         this.dcClient = dcClient;
@@ -29,5 +34,10 @@ public class ConsumerController {
     @GetMapping("/pwd")
     public String printConfiguration() {
         return value;
+    }
+
+    @GetMapping("/name")
+    public String printName() {
+        return name;
     }
 }
