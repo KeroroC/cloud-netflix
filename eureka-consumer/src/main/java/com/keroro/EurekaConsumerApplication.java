7eae4b583c34b235656fad725785f1ca51b95597
Keroro <keroro714@qq.com>
2024-01-25 15:05:56 +0800

    fix(eureka-consumer): 去除hystrix-dashboard依赖，保持单一职责

--- eureka-consumer/src/main/java/com/keroro/EurekaConsumerApplication.java
+++ eureka-consumer/src/main/java/com/keroro/EurekaConsumerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,7 +14,6 @@ import org.springframework.web.client.RestTemplate;
  * 这也意味着一个Spring Cloud标准应用应包含服务发现以及断路器
  */
 @EnableHystrix
-@EnableHystrixDashboard
 @EnableFeignClients
 @SpringCloudApplication
 public class EurekaConsumerApplication {
