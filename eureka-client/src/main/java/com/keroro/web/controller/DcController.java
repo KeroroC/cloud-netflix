562dcbb7627dd58d32589175bbff94c859cdab3a
Keroro <keroro714@qq.com>
2024-01-26 17:54:55 +0800

    build(eureka-client): 打印跟踪信息

--- eureka-client/src/main/java/com/keroro/web/controller/DcController.java
+++ eureka-client/src/main/java/com/keroro/web/controller/DcController.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author wangpeng
  * @since 2024年01月22日 19:01
@@ -22,8 +24,14 @@ public class DcController {
     }
 
     @GetMapping("/dc")
-    public String dc() {
+    public String dc(HttpServletRequest request) {
         logger.info("eureka-client，触发了/dc");
+        logger.info("=== traceContent: TraceId={}, SpanId={}, parentSpanId={}, sampled={}, spanName={} ===",
+                request.getHeader("X-B3-TraceId"),
+                request.getHeader("X-B3-SpanId"),
+                request.getHeader("X-B3-ParentSpanId"),
+                request.getHeader("X-B3-Sampled"),
+                request.getHeader("X-Span-Name"));
         String services = "Services: " + discoveryClient.getServices();
         System.out.println(services);
         return services;
